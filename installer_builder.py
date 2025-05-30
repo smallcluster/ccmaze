0511d345db527a77edf740312c53f9f70acb492a
Pierre Jaffuer <p.jaffuer@free.fr>
2025-05-30 21:32:08 +0400

    renamed example to demo

--- installer_builder.py
+++ installer_builder.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
 
     dirs = find_dirs(directory)
     files = find_files(directory)
-    files.append("example.lua")  # Add example.lua to the list of files
+    files.append("ccmaze-demo.lua")
 
     files_array = files_to_lua(files, "files")
     dirs_array = files_to_lua(dirs, "dirs")
